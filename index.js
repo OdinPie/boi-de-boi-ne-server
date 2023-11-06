e6c44ed616c22e0753f7f20b947158ab36a2e3ef
Maliha <maliha16tabassum@gmail.com>
2023-11-06 22:26:21 +0600

    added post api for add services

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // middleware
-
+//BookAdmin
+//OB2HduJbvc9GFk2k
 app.use(cors());
 app.use(express.json());
 
@@ -14,3 +16,38 @@ app.get('/', (req, res)=>{
 app.listen(port, ()=>{
     console.log(`listening on port : ${port}`);
 })
+
+
+const uri = "mongodb+srv://BookAdmin:OB2HduJbvc9GFk2k@odinpiesdatabase.beom3yx.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const database = client.db('bdbnDB');
+    const serviceCollection = database.collection('serviceCollection');
+
+    app.post('/services', async(req,res)=>{
+        const service = req.body;
+        const result = await serviceCollection.insertOne(service);
+        res.send(result);
+    })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
